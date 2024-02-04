07020123c93f62d6d72676a8e68787d1f5b8e401
lzhang2022 <l.zhang@heni.com>
2024-02-04 17:50:59 +0000

    update comments in run.sh

--- nd_feb2024/run.sh
+++ nd_feb2024/run.sh
@@ -3,11 +3,11 @@
 # install requirements
 pip install -r requirements.txt
 
+# use this command to find the location for ./path/to/spark/bin/spark-submit
 #find / -name "spark-submit" -type f 2>/dev/null
-# use this command above to find the location for ./path/to/spark/bin/spark-submit
 
 # Run spark-submit
-#./path/to/spark/bin/spark-submit \
-/Users/l.zhang/PycharmProjects/lz-pd-24/python10/lib/python3.10/site-packages/pyspark/bin/spark-submit \
+# Terminal be in directory nd_feb2024, bash run.sh to execute
+./path/to/spark/bin/spark-submit \
   --master local[*] \
   pipeline.py
